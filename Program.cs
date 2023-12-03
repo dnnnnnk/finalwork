86b6d77952017165ca996b8d13c4eade09aae969
yrth <komarvalera51@gmail.com>
2023-12-03 18:20:34 +0300

    написал программу

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string [] array = new string [5];
+
+string[] FillArray()
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+
+void PrintNewArray(string[] array)
+{
+    System.Console.WriteLine("новый массив: ");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            System.Console.Write($"{array[i]}\t");
+        }
+    }   
+}
+PrintNewArray(FillArray());
